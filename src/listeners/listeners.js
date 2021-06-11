1789916b733b8ae9f6d75c6153a77734ca0b2ca1
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-12 01:35:31 +0200

    data added

--- src/listeners/listeners.js
+++ src/listeners/listeners.js
@@ -1,7 +1,7 @@
-import { draw, mouseDown, notDrawing} from "../handlers/draw.js";
+import { draw, mouseDown, notDrawing } from "../handlers/draw.js";
+import { canvas } from "../data.js";
 
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', mouseDown);
-canvas.addEventListener('mouseup', notDrawing);
-canvas.addEventListener('mouseout', notDrawing);
-
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", mouseDown);
+canvas.addEventListener("mouseup", notDrawing);
+canvas.addEventListener("mouseout", notDrawing);
